32660a0450678f588761d458f2631beee478d266
pawanrock5756 <138184972+pawanrock5756@users.noreply.github.com>
2025-03-16 23:48:36 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+/*export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
 	// eslint-disable-next-line no-undef
-	plugins: [require("daisyui")],
+	//plugins: [require("daisyui")],
+};*/
+import daisyui from "daisyui";
+
+export default {
+  plugins: [daisyui],
 };
+
